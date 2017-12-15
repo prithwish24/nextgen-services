9daefa27cfc4ddb2a052509e5c318dd276ebcf63
Prithwish Ghosh <Prithwish.Ghosh@ehi.com>
2017-12-15 13:26:20 +0530

    Added basic coding for config module.

--- configuration/src/main/java/com/cts/product/config/Application.java
+++ configuration/src/main/java/com/cts/product/config/Application.java
@@ -1,13 +1,18 @@
 package com.cts.product.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import com.cts.product.config.controller.RefreshController;
+
 @SpringBootApplication
 @EnableConfigServer
+@EnableAutoConfiguration
+//@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(RefreshController.class, args);
 	}
 }
