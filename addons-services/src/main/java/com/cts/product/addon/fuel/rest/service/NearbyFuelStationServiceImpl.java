7c1500bd99d4ed145ed5cfca68b3e9d49bed36ab
tbiswas81 <Tanmoy.B81@gmail.com>
2018-01-03 18:01:52 +0530

    minor change

--- addons-services/src/main/java/com/cts/product/addon/fuel/rest/service/NearbyFuelStationServiceImpl.java
+++ addons-services/src/main/java/com/cts/product/addon/fuel/rest/service/NearbyFuelStationServiceImpl.java
@@ -3,7 +3,6 @@ package com.cts.product.addon.fuel.rest.service;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import com.cts.product.addon.fuel.model.FuelStationResponse;
 
@@ -14,9 +13,7 @@ public class NearbyFuelStationServiceImpl implements NearbyFuelStationService {
 	@Cacheable(value = "nearbyFuel", key = "#city")
 	public FuelStationResponse getNearbyFuelStations(String city) {
 		final String q = "api_key=DEMO_KEY&location=" + city;
-		final String uri = "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?";
-		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(uri).queryParam("q", q).queryParam("format",
-				"json");
+		final String uri = "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?";
 		RestTemplate restTemplate = new RestTemplate();
 		FuelStationResponse result = restTemplate.getForObject(uri + q, FuelStationResponse.class);
 
