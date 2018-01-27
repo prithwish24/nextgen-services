29cd2dd5399ab97535aa5cc0f02dfbecb9d9fb88
Prithwish Ghosh <Prithwish.Ghosh@cognizant.com>
2018-01-27 20:54:21 +0530

    Added Config Server Retry

--- monitoring/src/main/java/com/cts/product/monitor/Application.java
+++ monitoring/src/main/java/com/cts/product/monitor/Application.java
@@ -2,6 +2,7 @@ package com.cts.product.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
@@ -9,8 +10,9 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableHystrixDashboard
-@EnableTurbine
+//@EnableTurbine
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
